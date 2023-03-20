b46cf0687e546b99d1cbc8e64a254316ca3817fd
AP-lang <73804472+AP-lang@users.noreply.github.com>
2023-03-20 18:25:26 -0400

    Delete SQLQuery1.sql

--- SQLQuery1.sql
+++
@@ -1,5 +0,0 @@
-﻿Select name, Department, Salary,
-(select [Performance rating] From Performance_Review_T Where SSN= A.SSN) [Performance Rating]
-(Select [Percent Salary Increase] From Performance_Review_T Where SSN = A.SSN) [Percent Salary Increase]
-From Employee_T A
-Where position = 'worker'

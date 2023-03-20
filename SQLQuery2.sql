7197ed8ee1601e7a7a2795bbf85269afb2e3b5bf
AP-lang <73804472+AP-lang@users.noreply.github.com>
2023-03-20 18:25:35 -0400

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,4 +0,0 @@
-﻿Select SSN,
-(Select Name From Employee_T Where SSN = A.SSN) [worker Name], [Performance Rating], [Percent Salary Increase],
-(Select Name From Employee_T Where SSN = A.[Supervisor SSN]) [Supervisor Name]
-From performance_Review_t A

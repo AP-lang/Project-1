b928de598180d4973c2fa4d070cde9dc4a8f102c
AP-lang <73804472+AP-lang@users.noreply.github.com>
2023-03-20 18:20:05 -0400

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,4 @@
+﻿Select SSN,
+(Select Name From Employee_T Where SSN = A.SSN) [worker Name], [Performance Rating], [Percent Salary Increase],
+(Select Name From Employee_T Where SSN = A.[Supervisor SSN]) [Supervisor Name]
+From performance_Review_t A

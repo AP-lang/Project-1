163a5e165e7fb24a3b8238ffade33ad060afead7
AP-lang <73804472+AP-lang@users.noreply.github.com>
2023-03-20 18:20:49 -0400

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,10 @@
+﻿Update Performance_Review_T
+GO
+Set [Percent Salary Increase] = 4.2 
+Where SSN = 
+(Select SSN
+FROM  Employee_T inner join
+performance_review_T on employee_t.SSN = PERFORMANCE_REVIEW_T.SSN
+WHERE NAME = 'ALEX BROWN')
+GO 
+Select * from Performance_Review_T
